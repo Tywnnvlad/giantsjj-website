a4daf0f8aeac1a54e724ee8179c5ef1d494fcaf2
Gregory Cal <gcal@Laptop>
2023-08-02 16:21:08 -0600

    added sendgrid api

--- app/api/sendgrid.ts
+++ app/api/sendgrid.ts
@@ -2,6 +2,10 @@ import sendgrid from "@sendgrid/mail";
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
 
-async function name(params:type) {
-    
+async function sendEmail(req, res) {
+    try {
+        
+    } catch (error) {
+        
+    }
 }
