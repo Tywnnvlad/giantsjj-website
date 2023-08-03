cc873bbeb414047fa7219ca697f5fad2d6f14448
Gregory Cal <gregoryedward.cal@lab.kingsu.ca>
2023-08-03 00:52:33 -0600

    in the process of implementing sendgrid api

--- app/api/sendgrid.ts
+++ app/api/sendgrid.ts
@@ -1,21 +1,17 @@
-import sendgrid from "@sendgrid/mail";
+import sendgrid from '@sendgrid/mail';
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
-
 async function sendEmail(req, res) {
-    try {
-        await sendgrid.send({
-            to: "gred_cal@yahoo.com",
-            from: "gred_cal@yahoo.com",
-            subject: `${req.body.subject}`,
-            html:`<div>Hello World</div>`
-
-        });
-    } catch (error) {
-        return res.status(error.statusCode || 500).json({ error: error.message });
-
-    }
-    return res.status(200).json({ error: "" });
-
+	try {
+		await sendgrid.send({
+			to: 'gred_cal@yahoo.com',
+			from: 'gred_cal@yahoo.com',
+			subject: `${req.body.subject}`,
+			html: `<div>Hello World</div>`,
+		});
+	} catch (error) {
+		return res.status(error.statusCode || 500).json({ error: error.message });
+	}
+	return res.status(200).json({ error: '' });
 }
 export default sendEmail;
