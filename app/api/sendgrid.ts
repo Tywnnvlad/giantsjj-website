c0c09a110c21ee15bfb3a95d920f34857a580b4b
Gregory Cal <gregoryedward.cal@lab.kingsu.ca>
2023-08-03 01:15:13 -0600

    done writing the api, test the api tomorrow

--- app/api/sendgrid.ts
+++ app/api/sendgrid.ts
@@ -1,17 +1,45 @@
 import sendgrid from '@sendgrid/mail';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { stringify } from 'querystring';
+
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
-async function sendEmail(req, res) {
-	try {
-		await sendgrid.send({
+type Data = {
+	message: string;
+};
+
+interface emailstuff {
+	name: string;
+	email: string;
+	message: string;
+}
+
+async function sendEmail(req: NextApiRequest, res: NextApiResponse<Data>) {
+	if (req.method === 'POST') {
+		// idk what this is for maybe body?
+		const {
+			name,
+			email,
+			message,
+		}: { name: string; email: string; message: string } = req.body;
+
+		// setting data
+		const data = {
 			to: 'gred_cal@yahoo.com',
 			from: 'gred_cal@yahoo.com',
 			subject: `${req.body.subject}`,
 			html: `<div>Hello World</div>`,
-		});
-	} catch (error) {
-		return res.status(error.statusCode || 500).json({ error: error.message });
+		};
+
+		try {
+			await sendgrid.send(data);
+			res.status(200).json({ message: 'Message has been sent succesfully' });
+			console.log('msg sent lmao');
+		} catch (error) {
+			return res
+				.status(500)
+				.json({ message: `Problem sending the message ${err}` });
+		}
 	}
-	return res.status(200).json({ error: '' });
 }
 export default sendEmail;
