0e51b26cd34d1b1c031af206cc685fbb464c20ec
Gregory Cal <gregoryedward.cal@lab.kingsu.ca>
2023-08-08 22:47:52 -0600

    fixed the address and html block to send

--- pages/api/sendgrid.ts
+++ pages/api/sendgrid.ts
@@ -24,7 +24,33 @@ async function sendEmail(req: NextApiRequest, res: NextApiResponse<Data>) {
 			to: 'gred_cal@yahoo.com',
 			from: 'giantsjiujitsu@proton.me',
 			subject: `${req.body.subject}`,
-			html: `<div>Hello World</div>`,
+			html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+      <html lang="en">
+      <head>
+        <meta charset="utf-8">
+      
+        <title>The HTML5 Herald</title>
+        <meta name="description" content="The HTML5 Herald">
+        <meta name="author" content="SitePoint">
+      <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+      
+        <link rel="stylesheet" href="css/styles.css?v=1.0">
+      
+      </head>
+      
+      <body>
+
+	  <div class="container" style="margin-left: 20px;margin-right: 20px;">
+	  <h3>You've got a new mail from ${req.body.name}, their email is: ✉️${req.body.email} </h3>
+	  <div style="font-size: 16px;">
+	  <p>Message:</p>
+	  <p>${req.body.comment}</p>
+	  <br>
+	  </div>
+	  
+
+	  </body>
+      </html>`,
 		};
 
 		try {
