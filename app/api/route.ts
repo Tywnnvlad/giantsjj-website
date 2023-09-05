57ec1a6a18762fbecdf45c4203351c4b48718755
Gregory Cal <gregoryedward.cal@lab.kingsu.ca>
2023-09-04 20:39:10 -0600

    removed app/api/route.ts

--- app/api/route.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-	return new Response('Hello World!', {
-		status: 200,
-	});
-}
